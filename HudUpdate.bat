beb2f918b2cebe05d6c1d2aed5e1958eff85b375
Orangeprint <109125288+Orangeprint-git@users.noreply.github.com>
2023-01-26 08:39:52 +0200

    Update HudUpdate.bat

--- HudUpdate.bat
+++ HudUpdate.bat
@@ -8,6 +8,7 @@ set gitlink=https://github.com/Orangeprint-git/OrangeHUD.git
 set dllink=https://github.com/Orangeprint-git/OrangeHUD/archive/refs/heads/main.zip 
 set dllog=https://raw.githubusercontent.com/Orangeprint-git/OrangeHUD/main/UpdateLog.txt
 
+MOVE /y "UpdateLog.txt" "UpdateLogOld.txt"2>nul >nul
 
 powershell -Command "Invoke-WebRequest %dllog% -Outfile UpdateLog.txt"
 
@@ -63,9 +64,9 @@ if %errorlevel%==0 (
 for /f "tokens=1,* delims=:" %%c in ('findstr "UpdateVer" "UpdateLog.txt"') do set UpdateVer=%%d
 )
 
-findstr "UpdateVer" "UpdateLogIN.txt" 2>nul >nul
+findstr "UpdateVer" "UpdateLogOld.txt" 2>nul >nul
 if %errorlevel%==0 (
-for /f "tokens=1,* delims=:" %%g in ('findstr "UpdateVer" "UpdateLogIN.txt"') do set UpdateVer2=%%h
+for /f "tokens=1,* delims=:" %%g in ('findstr "UpdateVer" "UpdateLogOld.txt"') do set UpdateVer2=%%h
 )
 
 
@@ -105,7 +106,6 @@ goto startcls
 :yes
 @echo off
 SETLOCAL EnableExtensions
-MOVE /y "UpdateLog.txt" "UpdateLogIN.txt"2>nul >nul
 SET EXE=hl2.exe
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF NOT %%x == %EXE% (
   GOTO notRunning
